61f1b003cf10ef81f5bbb509bd119cde16bfaf3b
povea <povea_@outlook.com>
2023-08-31 22:36:34 -0400

    primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Taller_1"
+include(":app")
+ 
